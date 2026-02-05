52615a1592523d3d8efb9a0e2a818ecd7804fe18
Hamdi Kamel <hamdiakippuden@gmail.com>
2026-02-05 18:11:59 +0100

    refactor: enhance updateState logic and improve function wrapping in createApi

--- index.js
+++ index.js
@@ -1,50 +1,42 @@
 const createApi = (initialState = {}) => {
-    let currentState = {...initialState}
+    let currentState = Object.assign({}, initialState)
 
     return {
         getState: () => currentState,
         updateState(props) {
-            const newState = {...currentState}
+            const newState = Object.assign({}, currentState)
 
             for (const key in props)
                 newState[key] = typeof currentState[key] === 'function' && typeof props[key] === 'function' ?
-                    wrap(currentState[key], props[key]) : props[key]
+                    (function(prevFn, newFn) {
+                        return function(...args) {
+                            return newFn.apply(currentState, args.concat(prevFn))
+                        }
+                    })(currentState[key].bind(currentState), props[key]) : props[key]
 
             currentState = newState
         }
     }
-
-    function wrap(oldFn, newFn) {
-        const oldFnBound = oldFn.bind(currentState)
-        return (...args) => newFn.apply(currentState, args.concat(oldFnBound))
-    }
 }
 
 const api = createApi({
-    id: 1,
-    method() {
-        console.log(this.id)
+    generateRandomNumber(seed) {
+        return Math.floor(Math.random() * seed)
     }
 })
 
-// verifies the 'getState' method returns a snapshot of the state at the time it was defined.
-
-const oldState = api.getState()
-console.log(oldState)
+const snapshot1 = api.getState()
 
 api.updateState({
-    id: 2,
-    method(prevFn) {
-        prevFn()
-        console.log(this.id)
+    generateRandomNumber(seed, prevFn) {
+        if (!prevFn.cachedResult) prevFn.cachedResult = prevFn(seed)
+        console.log(prevFn.cachedResult)
     }
 })
 
-// verifies the getState method has access to the most current snapshot.
+const state = api.getState()
 
-const newState = api.getState()
-console.log(newState)
-
-// calls the method which has access to the previous implementation that holds a reference to the original state object.
-
-newState.method()
+state.generateRandomNumber(9)
+state.generateRandomNumber(9)
+state.generateRandomNumber(9)
+state.generateRandomNumber(9)
